844da5aeb86373f99efa4e8ed6414d2eeff17aa8
Nico Rithner <nicorithner@gmail.com>
2021-02-02 22:47:21 -0700

    Create mexican_wave.rb

---
+++ codewars/lib/mexican_wave.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class MexicanWave
+  def wave(string)
+    mx_wave = [] 
+    chr_arr = string.chars 
+    chr_arr.each_with_index do |e, i|
+      i != 0 && chr_arr[i - 1] = chr_arr.fetch(i - 1).downcase
+      next chr_arr[i] == " "
+      letter = chr_arr.delete_at(i)
+      chr_arr.insert(i,letter.upcase) 
+      mx_wave.push(chr_arr.join)
+    end
+    mx_wave
+  end
+
+end
