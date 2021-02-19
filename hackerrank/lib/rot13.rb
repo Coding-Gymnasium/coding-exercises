b2d4f60cd6636f91a6f4983dd0c988f5985c6bb1
Nico Rithner <nicorithner@gmail.com>
2021-02-19 10:44:15 -0700

    Add another solution. Still not passing hackerrank test

--- hackerrank/lib/rot13.rb
+++ hackerrank/lib/rot13.rb
@@ -2,21 +2,45 @@
 #
 
 class Rot13
-  @@dictionary = {}
-
-  def set_dictionary
-    alph = Array('a'..'z')
-    r13_alph = alph.rotate(13)
-    alph.each { |letter| @@dictionary[letter] = r13_alph[alph.index(letter)] }
-  end
-
   def rot13(secret_messages)
-    set_dictionary
-    brokened_up = secret_messages.map { |string| string.downcase.chars }
-    translated =  brokened_up.map do |arr|
-      arr.map { |letter| letter == " " || letter =~ /[?.!]/ ? letter : letter = @@dictionary[letter] }
-    end
-    translated.map {|arr| arr.join.capitalize }
+      dictionary = {}
+      alph = Array('a'..'z')
+      r13_alph = alph.rotate(13)
+      alph.each { |letter| dictionary[letter] = r13_alph[alph.index(letter)] }
+    
+      brokened_up = secret_messages.map { |string| string.downcase.chars }
+      translated =  brokened_up.map do |arr|
+      arr.map { |letter| letter == " " || letter =~ /[?.!]/ ? letter : letter = dictionary[letter] }
+      end
+      translated.map {|arr| arr.join.capitalize }
   end
 end
+
+#def rot13(secret_messages)
+#    alphabet = Array('a'..'z')
+#    secret_messages.map do |str|
+#      arr = str.chars
+#       translated = arr.map { |letter| letter = alphabet.rotate(13) if alphabet.include?(letter) }
+#       translated
+#    end
+#end
+
+#class Rot13
+#  @@dictionary = {}
+#
+#  def set_dictionary
+#    alph = Array('a'..'z')
+#    r13_alph = alph.rotate(13)
+#    alph.each { |letter| @@dictionary[letter] = r13_alph[alph.index(letter)] }
+#  end
+#
+#  def rot13(secret_messages)
+#    set_dictionary
+#    brokened_up = secret_messages.map { |string| string.downcase.chars }
+#    translated =  brokened_up.map do |arr|
+#      arr.map { |letter| letter == " " || letter =~ /[?.!]/ ? letter : letter = @@dictionary[letter] }
+#    end
+#    translated.map {|arr| arr.join.capitalize }
+#  end
+#end
 
