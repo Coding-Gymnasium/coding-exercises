24dfeed802388d01e3c7b1f725d9bde36b9f7e24
Nico Rithner <nicorithner@gmail.com>
2021-02-18 20:32:42 -0700

    Change tests

--- leetcode/test/missing_number_spec.rb
+++ leetcode/test/missing_number_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe MissingNumber do
   before :each do
     @mnum = MissingNumber.new
   end
+  
   it "Returns the missing number" do
     num = [1,2,3]
-    expect(@mnum.missing_number(num)).to eq('no missing number')
+    expect(@mnum.missing_number(num)).to eq(0)
   end
 
   it "" do
@@ -24,6 +25,10 @@ RSpec.describe MissingNumber do
     nums2 = [9,6,4,2,3,5,7,0,1]
     expect(@mnum.missing_number(nums2)).to eq(8)
   end
-
+  
+  it "" do
+    nums3 = [0]
+    expect(@mnum.missing_number(nums3)).to eq(1)
+  end
 end
 #https://leetcode.com/problems/missing-number/
