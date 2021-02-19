cabb02ad2d637d2bc5f1d3125907af54565cb5b1
Nico Rithner <nicorithner@gmail.com>
2021-02-19 09:56:23 -0700

    Update tests

--- hackerrank/test/rot13_spec.rb
+++ hackerrank/test/rot13_spec.rb
@@ -1,18 +1,15 @@
 require_relative '../lib/rot13'
+require 'pry'
 
 RSpec.describe Rot13 do
   before :each do
     @rot = Rot13.new
   end
 
-  it "translates from english to Rot13" do
-    secret_message = 'Why did the chicken cross the road?'
-    expect(@rot.rot13(secret_message)).to eq('Jul qvq gur puvpxra pebff gur ebnq?')
-  end
+  it "translates secrete code from english to Rot13 and viceversa" do
 
-  it "translates from rot13 to english" do
-    secret_message = 'Gb trg gb gur bgure fvqr!'
-    expect(@rot.english(secret_message)).to eq('To get to the other side!')
+    secret_message = ['Why did the chicken cross the road?', 'Gb trg gb gur bgure fvqr!']
+    expect(@rot.rot13(secret_message)).to eq(['Jul qvq gur puvpxra pebff gur ebnq?', 'To get to the other side!'])
   end
 
 end
