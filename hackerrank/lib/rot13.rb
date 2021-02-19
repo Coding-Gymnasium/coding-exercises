c2d5df8a490cb58bb90a1bd3b3b101eec432e3dd
Nico Rithner <nicorithner@gmail.com>
2021-02-19 09:56:39 -0700

    Solve challenge

--- hackerrank/lib/rot13.rb
+++ hackerrank/lib/rot13.rb
@@ -1,22 +1,22 @@
-class Rot13
-  def initialize
-    @dictionary = {}
-    set_dictionary
-    require 'pry'; binding.pry
+#write a method which takes an array of strings (containing secret enemy message bits!) and decodes its elements using ROT13 cipher system; returning an array containing the final messages.
+#
 
-  end
-  
+class Rot13
+  @@dictionary = {}
+
   def set_dictionary
     alph = Array('a'..'z')
     r13_alph = alph.rotate(13)
-    alph.each { |letter| @dictionary[letter] = r13_alph[alph.index(letter)] }
+    alph.each { |letter| @@dictionary[letter] = r13_alph[alph.index(letter)] }
   end
-  
+
   def rot13(secret_messages)
+    set_dictionary
+    brokened_up = secret_messages.map { |string| string.downcase.chars }
+    translated =  brokened_up.map do |arr|
+      arr.map { |letter| letter == " " || letter =~ /[?.!]/ ? letter : letter = @@dictionary[letter] }
+    end
+    translated.map {|arr| arr.join.capitalize }
   end
-
-  def english(secret_messages)
-  end
-
 end
 
