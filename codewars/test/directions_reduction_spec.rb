9a6cab8e792068ffd162d508c6e4be84623b6266
Nico Rithner <nicorithner@gmail.com>
2021-02-28 09:40:32 -0700

    Move solved challenges to 'solved' directory
    Created 'solved' directory

---
+++ codewars/test/directions_reduction_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/directions_reduction'
+require 'pry'
+
+RSpec.describe DirectionsReduction do
+  before :each do
+    @dr = DirectionsReduction.new
+  end
+
+  it 'Can remove redundant directions' do
+
+  end
+end
