a6b2ea799ad951d345cecc6d33916f1928cc7a38
Nico Rithner <nicorithner@gmail.com>
2021-03-16 10:32:26 -0600

    Update test. Reach partial solution. Not completed

--- codewars/lib/two_sum.rb
+++ codewars/lib/two_sum.rb
@@ -1,10 +1,18 @@
 class TwoSum
-  def two_sum(arr, target)
+  def two_sum(numbers, target)
+    result = []
+    numbers.find do |element|
+      substraction = target - element
+      indexes = []
+      numbers.each_with_index { |num, ind| num == element && indexes.push(ind) }
+      numbers.include?(substraction) && result.push(numbers.index(substraction)) && result.push(numbers.index(element))
+    end
+    result
   end
 end
 
-#Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+# Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
 
-#For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+# For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
 
-#The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+# The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
