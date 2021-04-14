9db4a2f8821cb87d10d729be97b257216db2856a
Nico Rithner <nicorithner@gmail.com>
2021-04-14 13:13:14 -0600

    Solve Two_sum

--- codewars/ruby/lib/solved/two_sum.rb
+++ codewars/ruby/lib/solved/two_sum.rb
@@ -1,16 +1,23 @@
 class TwoSum
   def two_sum(numbers, target)
     result = []
-    numbers.find do |element|
-      substraction = target - element
-      if numbers.include?(substraction) 
-        result.push(numbers.index(substraction)) 
-        numbers.each_with_index { |num, ind| result.push(numbers.index(ind)) if num == element }
+    numbers.each_with_index do |n, i|
+      n2 = target - n
+      if numbers.include?(n2) && result.push(i)
+        numbers.each_with_index { |n3, i3| n3 == n2 && i3 != i && result.push(i3) } && break
       end
     end
+
     result
   end
 end
+    # numbers.find do |element|
+    #   substraction = target - element
+    #   if numbers.include?(substraction) 
+    #     result.push(numbers.index(substraction)) 
+    #     numbers.each_with_index { |num, ind| result.push(numbers.index(ind)) if num == element }
+    #   end
+    # end
 
 # Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
 
