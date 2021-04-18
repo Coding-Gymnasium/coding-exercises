14baa1f46d2b6000e867865c49ad4fa7bc18ecef
Nico Rithner <nicorithner@gmail.com>
2021-04-18 10:37:48 -0600

    Set up uniqueInOrder js challenge

---
+++ codewars/js/test/uniqueInOrder.test.js
@@ -0,0 +1,7 @@
+const uniqueInOrder = require('../lib/uniqueInOrder');
+
+test('It returns a list of items without any elements with the same value next to each other and preserving the original order of elements', () => {
+  expect(uniqueInOrder('AAAABBBCCDAABBB')).toBe(['A', 'B', 'C', 'D', 'A', 'B'])
+  expect(uniqueInOrder('ABBCcAD')).toBe(['A', 'B', 'C', 'c', 'A', 'D'])
+  expect(uniqueInOrder('1,2,2,3,3')).toBe(['1', '2', '3'])
+});
