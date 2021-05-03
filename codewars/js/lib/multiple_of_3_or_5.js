076cc267017bc0c8b8ee92574c6cbede0a86067a
Nico Rithner <nicorithner@gmail.com>
2021-05-03 10:22:10 -0600

    Solve edges cases tests for multiple_of.. challenge

--- codewars/js/lib/multiple_of_3_or_5.js
+++ codewars/js/lib/multiple_of_3_or_5.js
@@ -1,7 +1,11 @@
 function solution(number) {
   let numbers = [];
-  for (const x of Array(number).keys()) { (x % 3 === 0 && numbers.push(x)) || (x % 5 === 0 && numbers.push(x))}
-  return numbers.reduce((a, b) => a + b, 0)
+  if(number < 0) {
+    return 0;
+  } else {
+    for (const x of Array(number).keys()) { (x % 3 === 0 && numbers.push(x)) || (x % 5 === 0 && numbers.push(x))}
+    return numbers.reduce((a, b) => a + b, 0)
+  }
 }
 module.exports = solution;
 
--- codewars/js/test/multiple_of_3_or_5.test.js
+++ codewars/js/test/multiple_of_3_or_5.test.js
@@ -4,4 +4,5 @@ test('It returns the sum of all multiples of 3 or 5 within the range between zer
   expect(solution(10)).toBe(23)
   expect(solution(20)).toBe(78)
   expect(solution(200)).toBe(9168)
+  expect(solution(-2)).toBe(0)
 });
