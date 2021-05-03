869ef411339494c9a0b32afd607cf60a2448f9f7
Nico Rithner <nicorithner@gmail.com>
2021-05-02 18:42:32 -0600

    Solve multiple_of_3_or_5 but doesn't pass 'Attempt'

--- codewars/js/lib/multiple_of_3_or_5.js
+++ codewars/js/lib/multiple_of_3_or_5.js
@@ -1,4 +1,7 @@
 function solution(number) {
-  console.log(number);
-  
+  let numbers = [];
+  for (const x of Array(number).keys()) { (x % 3 === 0 && numbers.push(x)) || (x % 5 === 0 && numbers.push(x))}
+  return numbers.reduce((a, b) => a + b, 0)
 }
+module.exports = solution;
+
