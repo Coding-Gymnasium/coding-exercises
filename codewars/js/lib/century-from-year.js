90e4c5fbd62f8a06004e77d67c3e8f08fc906272
Nico Rithner <nicorithner@gmail.com>
2021-11-20 09:41:41 -0700

    Set up codewars challenge 'century from year'

---
+++ codewars/js/lib/century-from-year.js
@@ -0,0 +1,5 @@
+const century = (year) => {
+   
+};
+
+module.exports = century;
---
+++ codewars/js/test/century-from-year.test.js
@@ -0,0 +1,11 @@
+const century = require('../lib/century-from-year');
+
+describe('Century from a give year', () => {
+  it('the correct century number is returned', () => {
+    expect(century(1705)).toBe(18);
+    expect(century(1900)).toBe(19);
+    expect(century(1601)).toBe(17);
+    expect(century(2000)).toBe(20);
+    expect(century(89)).toBe(1);
+  });
+});
