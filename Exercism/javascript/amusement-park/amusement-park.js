88bfd52e31ce59d29502f7cdf8b86ce875d74197
Nico Rithner <nicorithner@gmail.com>
2022-09-23 09:01:52 -0600

    Solve get version of term and conditions

--- Exercism/javascript/amusement-park/amusement-park.js
+++ Exercism/javascript/amusement-park/amusement-park.js
@@ -60,5 +60,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error("Please implement the gtcVersion function.");
+  return visitor.gtc?.version
 }
