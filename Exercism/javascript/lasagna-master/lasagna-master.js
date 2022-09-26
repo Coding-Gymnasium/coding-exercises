02619be41225e32dd466debd2ab9b9164e3379a1
Nico Rithner <nicorithner@gmail.com>
2022-09-26 11:42:46 -0600

    Set lasagna-master exercise. Solve
    cookingStatus function test

---
+++ Exercism/javascript/lasagna-master/lasagna-master.js
@@ -0,0 +1,27 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+
+export function cookingStatus(time) {
+  switch (true) {
+    case time > 0:
+      return 'Not done, please wait.';
+      break;
+    case time === 0:
+      return 'Lasagna is done.';
+      break;
+
+    default:
+      return time ??= 'You forgot to set the timer.'
+      break;
+  }
+}
