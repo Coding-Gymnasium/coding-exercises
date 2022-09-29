d1fa62a96ee9972fdbb9adb13146be90a198fbcf
Nico Rithner <nicorithner@gmail.com>
2022-09-29 10:29:46 -0600

    Upgrade solution

--- Exercism/javascript/luhn/luhn.js
+++ Exercism/javascript/luhn/luhn.js
@@ -4,6 +4,10 @@
 
 // Conditions
 // str length <= 1 is not valid.
+// Single 0 is not valid
+// Divisble by 10
+
+// Steps
 // Convert to array and remove empty spaces.
 // Convert each element to number
 // starting from the back. double every 2nd digit. If the result is greater
@@ -11,29 +15,39 @@
 // Add up all the digits
 // check the result is divisible by 10
 
-export const valid = (str) => {
-  // convert to array.
-  // reverse array
-  // filter blank spaces
-  let revArr = str
+const convertToReversedArray = (str) => {
+  return str
     .split("")
     .reverse()
     .filter((x) => x != " ");
+};
+
+const convertToNumber = (arr) => arr.map((el) => +el);
+
+const doubleEveryOtherAndAdjust = (arrNum) => {
+  for (let i = 1; i < arrNum.length; i += 2) {
+    let dbl = arrNum[i] * 2;
+    dbl > 9 ? (arrNum[i] = dbl - 9) : (arrNum[i] = dbl);
+  }
+  return arrNum;
+};
+
+const totalSum = (arrNum) => arrNum.reduce((ag, num) => ag + num);
+
+export const valid = (str) => {
+  let revArr = convertToReversedArray(str);
 
   // check for length less or equal 1
   if (revArr.length <= 1) return false;
 
   // convert to numbers
-  let arrNum = revArr.map(el => +el);
+  let arrNum = convertToNumber(revArr);
 
   // double every second digit and adjust if greater than 9
-  for (let i = 1; i < arrNum.length; i += 2) {
-    let dbl = arrNum[i] * 2;
-    dbl > 9 ? (arrNum[i] = dbl - 9) : (arrNum[i] = dbl);
-  }
+  doubleEveryOtherAndAdjust(arrNum);
 
   // add up all digits
-  let arrSum = arrNum.reduce((ag, num) => ag + num);
+  let arrSum = totalSum(arrNum);
 
   // is divisible by 10
   return arrSum % 10 === 0;
