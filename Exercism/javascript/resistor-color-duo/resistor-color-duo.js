5156591ac035cbe6cd3584d48d7eb2ef4835c8b7
Nico Rithner <nicorithner@gmail.com>
2023-03-11 07:55:55 -0700

    Solved decodedValue exercise

--- Exercism/javascript/resistor-color-duo/resistor-color-duo.js
+++ Exercism/javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this statement and implement this function');
+const colorValues = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const decodedValue = (values) => {
+  return +`${colorValues[values[0]]}${colorValues[values[1]]}`;
 };
