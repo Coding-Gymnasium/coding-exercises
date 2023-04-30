a63d23c52091492da90fc1ffd867add5ddd40db0
Nico Rithner <nicorithner@gmail.com>
2023-04-30 09:54:00 -0600

    Solve can signal prisioner exercise

--- Exercism/JAVA/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ Exercism/JAVA/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -7,22 +7,27 @@ class AnnalynsInfiltration {
 
   public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake,
                                boolean prisonerIsAwake) {
-        // the group can be spied upon if at least one of them is awake.
-        // Otherwise, spying is a waste of time
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    // the group can be spied upon if at least one of them is awake.
+    // Otherwise, spying is a waste of time
+    return knightIsAwake || archerIsAwake || prisonerIsAwake;
   }
 
   public static boolean canSignalPrisoner(boolean archerIsAwake,
                                           boolean prisonerIsAwake) {
-        throw new UnsupportedOperationException(
-            "Please implement the (static) AnnalynsInfiltration.canSignalPrisoner() method");
+    /**
+     * the prisoner can be signalled using bird sounds if the prisoner is awake
+     * and the archer is sleeping, as archers are trained in bird signaling, so
+     * they could intercept the message.
+     */
+
+    return !archerIsAwake && prisonerIsAwake;
   }
 
   public static boolean canFreePrisoner(boolean knightIsAwake,
                                         boolean archerIsAwake,
                                         boolean prisonerIsAwake,
                                         boolean petDogIsPresent) {
-        throw new UnsupportedOperationException(
-            "Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
+    throw new UnsupportedOperationException(
+        "Please implement the (static) AnnalynsInfiltration.canFreePrisoner() method");
   }
 }
