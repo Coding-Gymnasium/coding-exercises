0a36c68e6d92a416e1fe41c701b1992881dbd330
Nico Rithner <nicorithner@gmail.com>
2023-04-30 17:46:45 -0600

    Complete blackjack challenge

--- Exercism/JAVA/blackjack/src/main/java/Blackjack.java
+++ Exercism/JAVA/blackjack/src/main/java/Blackjack.java
@@ -2,54 +2,85 @@ import java.util.HashMap;
 
 public class Blackjack {
 
-  public int parseCard(String card) {
-    HashMap<String, Integer> cards = new HashMap<String, Integer>();
+    public int parseCard(String card) {
+        HashMap<String, Integer> cards = new HashMap<String, Integer>();
 
-    cards.put("ace", 11);
-    cards.put("eight", 8);
-    cards.put("two", 2);
-    cards.put("nine", 9);
-    cards.put("three", 3);
-    cards.put("ten", 10);
-    cards.put("four", 4);
-    cards.put("jack", 10);
-    cards.put("five", 5);
-    cards.put("queen", 10);
-    cards.put("six", 6);
-    cards.put("king", 10);
-    cards.put("seven", 7);
-    cards.put("other", 0);
+        cards.put("ace", 11);
+        cards.put("eight", 8);
+        cards.put("two", 2);
+        cards.put("nine", 9);
+        cards.put("three", 3);
+        cards.put("ten", 10);
+        cards.put("four", 4);
+        cards.put("jack", 10);
+        cards.put("five", 5);
+        cards.put("queen", 10);
+        cards.put("six", 6);
+        cards.put("king", 10);
+        cards.put("seven", 7);
+        cards.put("other", 0);
 
-    return cards.get(card);
-  }
-
-  public boolean isBlackjack(String card1, String card2) {
-    //  Blackjack (two cards that sum up to a value of 21)
-    return parseCard(card1) + parseCard(card2) == 21;
-  }
-
-  public String largeHand(boolean isBlackjack, int dealerScore) {
-    throw new UnsupportedOperationException(
-        "Please implement the Blackjack.largeHand method");
-  }
-
-  public String smallHand(int handScore, int dealerScore) {
-    throw new UnsupportedOperationException(
-        "Please implement the Blackjack.smallHand method");
-  }
-
-  // FirstTurn returns the semi-optimal decision for the first turn, given the
-  // cards of the player and the dealer. This function is already implemented
-  // and does not need to be edited. It pulls the other functions together in a
-  // complete decision tree for the first turn.
-  public String firstTurn(String card1, String card2, String dealerCard) {
-    int handScore = parseCard(card1) + parseCard(card2);
-    int dealerScore = parseCard(dealerCard);
-
-    if (20 < handScore) {
-      return largeHand(isBlackjack(card1, card2), dealerScore);
-    } else {
-      return smallHand(handScore, dealerScore);
+        return cards.get(card);
     }
-  }
+
+    public boolean isBlackjack(String card1, String card2) {
+        //  Blackjack (two cards that sum up to a value of 21)
+        return parseCard(card1) + parseCard(card2) == 21;
+    }
+
+    /**
+     * Category: Large Hand
+     * If you have a pair of aces you must always split them.
+     * <p>
+     * If you have a Blackjack (two cards that sum up to a value of 21), and
+     * the dealer does not have an ace, a figure or a ten then you
+     * automatically win.
+     * <p>
+     * If the dealer does have any of those cards then
+     * you'll have to stand and wait for the reveal of the other card.
+     */
+
+    public String largeHand(boolean isBlackjack, int dealerScore) {
+        if (isBlackjack && !(dealerScore >= 10)) {
+            return "W";
+        } else if (!isBlackjack) {
+            return "P";
+        } else {
+            return "S";
+        }
+    }
+
+    /**
+     * Category: Small Hand
+     * If your cards sum up to 17 or higher you should always stand.
+     * If your cards sum up to 11 or lower you should always hit.
+     * If your cards sum up to a value within the range [12, 16] you should
+     * always stand unless the dealer has a 7 or higher, in which case you
+     * should always hit.
+     */
+
+    public String smallHand(int handScore, int dealerScore) {
+        boolean range = handScore >= 12 && handScore <= 16;
+
+        if (handScore >= 17 || range && dealerScore < 7) {
+            return "S";
+        } else {
+            return "H";
+        }
+    }
+
+    // FirstTurn returns the semi-optimal decision for the first turn, given the
+    // cards of the player and the dealer. This function is already implemented
+    // and does not need to be edited. It pulls the other functions together in a
+    // complete decision tree for the first turn.
+    public String firstTurn(String card1, String card2, String dealerCard) {
+        int handScore = parseCard(card1) + parseCard(card2);
+        int dealerScore = parseCard(dealerCard);
+
+        if (20 < handScore) {
+            return largeHand(isBlackjack(card1, card2), dealerScore);
+        } else {
+            return smallHand(handScore, dealerScore);
+        }
+    }
 }
--- Exercism/JAVA/blackjack/src/test/java/BlackjackTest.java
+++ Exercism/JAVA/blackjack/src/test/java/BlackjackTest.java
@@ -129,193 +129,161 @@ public class BlackjackTest {
         assertThat(blackjack.isBlackjack("eight", "five")).isEqualTo(false);
     }
    
-    @Ignore
     @Test
     public void firstTurnWithAceAceAndDealerAce () {
         assertThat(blackjack.firstTurn("ace", "ace", "ace")).isEqualTo("P");
     }
    
-    @Ignore
     @Test
     public void firstTurnWithJackJackAndDealerAce () {
         assertThat(blackjack.firstTurn("jack", "jack", "ace")).isEqualTo("S");
     }
    
-    @Ignore
     @Test
     public void firstTurnWithKingKingAndDealerAce () {
         assertThat(blackjack.firstTurn("king", "king", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoTwoAndDealerAce () {
         assertThat(blackjack.firstTurn("two", "two", "ace")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFiveFiveAndAce () {
         assertThat(blackjack.firstTurn("five", "five", "ace")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithJackAceAndDealerAce () {
         assertThat(blackjack.firstTurn("jack", "ace", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithAceKingAndDealerQueen () {
         assertThat(blackjack.firstTurn("ace", "king", "queen")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTenAceAndDealerFive () {
         assertThat(blackjack.firstTurn("ten", "ace", "five")).isEqualTo("W");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithKingAceAndDealerNine () {
         assertThat(blackjack.firstTurn("king", "ace", "nine")).isEqualTo("W");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithKingTenAndDealerAce () {
         assertThat(blackjack.firstTurn("king", "ten", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithNineTenAndDealerAce () {
         assertThat(blackjack.firstTurn("nine", "ten", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithEightTenAndDealerAce () {
         assertThat(blackjack.firstTurn("eight", "ten", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithKingSevenAndDealerAce () {
         assertThat(blackjack.firstTurn("king", "seven", "ace")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithSixTenAndDealerSix () {
         assertThat(blackjack.firstTurn("six", "ten", "six")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithSixTenAndDealerSeven () {
         assertThat(blackjack.firstTurn("six", "ten", "seven")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithSixTenAndDealerAce () {
         assertThat(blackjack.firstTurn("six", "ten", "ace")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFiveTenAndDealerSix () {
         assertThat(blackjack.firstTurn("five", "ten", "six")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFiveTenAndDealerSeven () {
         assertThat(blackjack.firstTurn("five", "ten", "seven")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFiveTenAndDealerKing () {
         assertThat(blackjack.firstTurn("five", "ten", "king")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFourTenAndDealerSix () {
         assertThat(blackjack.firstTurn("four", "ten", "six")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFourTenAndDealerSeven () {
         assertThat(blackjack.firstTurn("four", "ten", "seven")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithFourTenAndDealerQueen () {
         assertThat(blackjack.firstTurn("four", "ten", "queen")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithThreeTenAndDealerSix () {
         assertThat(blackjack.firstTurn("three", "ten", "six")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithThreeTenAndDealerSeven () {
         assertThat(blackjack.firstTurn("three", "ten", "seven")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithThreeTenAndDealerQueen () {
         assertThat(blackjack.firstTurn("three", "ten", "queen")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoTenAndDealerSix () {
         assertThat(blackjack.firstTurn("two", "ten", "six")).isEqualTo("S");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoTenAndDealerSeven () {
         assertThat(blackjack.firstTurn("two", "ten", "seven")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoTenAndDealerQueen () {
         assertThat(blackjack.firstTurn("two", "ten", "queen")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoNineAndDealerQueen () {
         assertThat(blackjack.firstTurn("two", "nine", "queen")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoEightAndDealerTwo () {
         assertThat(blackjack.firstTurn("two", "eight", "two")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoThreeAndDealerQueen () {
         assertThat(blackjack.firstTurn("two", "three", "queen")).isEqualTo("H");
     }
 
-    @Ignore
     @Test
     public void firstTurnWithTwoTwoAndDealerFive () {
         assertThat(blackjack.firstTurn("two", "two", "five")).isEqualTo("H");
