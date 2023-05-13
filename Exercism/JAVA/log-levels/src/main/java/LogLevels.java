e620297e7d4157642773dfddc11e287e7b7a5026
Nico Rithner <nicorithner@gmail.com>
2023-05-13 16:57:59 -0600

    Complete log-level challenge

--- Exercism/JAVA/log-levels/src/main/java/LogLevels.java
+++ Exercism/JAVA/log-levels/src/main/java/LogLevels.java
@@ -1,16 +1,23 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-      String[] splitLogLine = logLine.split(":");
+      String[] splitLogLine = splitString(logLine);
       return splitLogLine[splitLogLine.length - 1].trim();
     }
 
     public static String logLevel(String logLine) {
-        String logLevel = logLine.split(":")[0].toLowerCase();
+        String logLevel = splitString(logLine)[0].toLowerCase();
         return logLevel.replace("[", "").replace("]", "");
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        String[] splitLogLine = splitString(logLine);
+        String level = splitLogLine[0].replace("[", "(").replace("]", ")").toLowerCase();
+        String message = splitLogLine[splitLogLine.length - 1].trim();
+        return message + " " + level;
+    }
+
+    public static String[] splitString(String string) {
+       return string.split(":");
     }
 }
