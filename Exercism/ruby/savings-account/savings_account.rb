2199098587bfc0c09b314ab8bdc887338d8904c6
Nico Rithner <nicorithner@gmail.com>
2023-08-20 13:00:22 -0600

    Solve interest_rate method in savings_account

--- Exercism/ruby/savings-account/savings_account.rb
+++ Exercism/ruby/savings-account/savings_account.rb
@@ -1,13 +1,25 @@
+# frozen_string_literal: true
+
+# Savings account module
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    case true
+    when balance < 1000 && balance >= 0
+      0.5
+    when balance < 5000 && balance >= 1000
+      1.621
+    when balance >= 5000
+      2.475
+    else
+      3.213
+    end
   end
 
-  def self.annual_balance_update(balance)
+  def self.annual_balance_update(_balance)
     raise 'Please implement the SavingsAccount.annual_balance_update method'
   end
 
-  def self.years_before_desired_balance(current_balance, desired_balance)
+  def self.years_before_desired_balance(_current_balance, _desired_balance)
     raise 'Please implement the SavingsAccount.years_before_desired_balance method'
   end
 end
