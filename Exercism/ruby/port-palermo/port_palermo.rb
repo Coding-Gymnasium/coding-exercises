c3a97cc69665a1a5e9466f7d8c108e29c44364a7
Nico Rithner <nicorithner@gmail.com>
2023-08-21 17:14:24 -0600

    Complete exercise 1 and 2

--- Exercism/ruby/port-palermo/port_palermo.rb
+++ Exercism/ruby/port-palermo/port_palermo.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
+# Port module
 module Port
   # TODO: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    truncated_name = city.slice(0..3).upcase
+    truncated_name.to_sym
   end
 
-  def self.get_terminal(ship_identifier)
+  def self.get_terminal(_ship_identifier)
     raise 'Please implement the Port.get_terminal method'
   end
 end
