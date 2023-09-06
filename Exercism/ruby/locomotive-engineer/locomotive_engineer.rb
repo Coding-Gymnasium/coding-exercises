6042cef2e4ce6d3ff73587f53cd29b0cc45e76d5
Nico Rithner <nicorithner@gmail.com>
2023-09-06 08:18:41 -0600

    Set up locomotive_engineer exercise

--- Exercism/ruby/locomotive-engineer/locomotive_engineer.rb
+++ Exercism/ruby/locomotive-engineer/locomotive_engineer.rb
@@ -1,9 +1,9 @@
 class LocomotiveEngineer
   def self.generate_list_of_wagons
-    raise 'Please implement the LocomotiveEngineer.generate_list_of_wagons method'
+
   end
 
-  def self.fix_list_of_wagons(each_wagons_id, missing_wagons)
+  def self.fix_list_of_wagons(_each_wagons_id, _missing_wagons)
     raise 'Please implement the LocomotiveEngineer.fix_list_of_wagons method'
   end
 
@@ -11,7 +11,7 @@ class LocomotiveEngineer
     raise 'Please implement the LocomotiveEngineer.add_missing_stops method'
   end
 
-  def self.extend_route_information(route, more_route_information)
+  def self.extend_route_information(_route, _more_route_information)
     raise 'Please implement the LocomotiveEngineer.extend_route_information method'
   end
 end
