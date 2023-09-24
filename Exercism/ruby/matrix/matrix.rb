da8f4ee0ea2a8e89aace65c9ded4bb28ce92b320
Nico Rithner <nicorithner@gmail.com>
2023-09-23 21:32:08 -0600

    Start solving matrix

--- Exercism/ruby/matrix/matrix.rb
+++ Exercism/ruby/matrix/matrix.rb
@@ -5,3 +5,16 @@
 #
 # To get started with TDD, see the `README.md` file in your
 # `ruby/matrix` directory.
+class Matrix
+  def initialize(matrix)
+    @matrix = []
+    create_matrix("1 2\n3 4")
+  end
+
+  private
+
+  def create_matrix(matrix)
+    char_arr = matrix.gsub(/ /, '').chars
+    print char_arr
+  end
+end
