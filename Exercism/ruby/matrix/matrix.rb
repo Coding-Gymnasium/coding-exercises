329d6f0a96d9dc20fb17ea897147c6e1d74ad285
Nico Rithner <nicorithner@gmail.com>
2023-09-24 17:42:58 -0600

    Solve multiple rows case

--- Exercism/ruby/matrix/matrix.rb
+++ Exercism/ruby/matrix/matrix.rb
@@ -20,15 +20,31 @@ class Matrix
   def create_matrix(matrix)
     char_arr = matrix.delete(' ').chars
 
-    # one row
+    #-- one row
     int_arr = char_arr.map(&:to_i) unless char_arr.include?("\n")
     @matrix.push(int_arr)
 
-    # multiple rows
+    #-- multiple rows
     # iterate over array.
     # gather numbers that come before "\n" already converted to int into an array.
     # push that array into a temp array.
     # push last group that come after "\n" to temp array too.
     # push temp array to @matrix
+
+    temp = []
+    row = []
+    char_arr.map do |char|
+      index = char_arr.index(char)
+      if index == char_arr.length - 1
+        row.push(char.to_i)
+        temp.push(row)
+      elsif char != "\n"
+        row.push(char.to_i)
+      elsif char == "\n"
+        temp.push(row)
+        row = []
+      end
+    end
+    @matrix = temp
   end
 end
