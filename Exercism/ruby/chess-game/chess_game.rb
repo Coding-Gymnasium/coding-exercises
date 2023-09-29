94024b3bc3df513510387c21fe07eea15b24702d
Nico Rithner <nicorithner@gmail.com>
2023-09-28 19:44:39 -0600

    Create RANKS and FILES variables

---
+++ Exercism/ruby/chess-game/chess_game.rb
@@ -0,0 +1,19 @@
+module Chess
+  # TODO: define the 'RANKS' constant
+  RANKS = 1..8
+  p RANKS
+  # TODO: define the 'FILES' constant
+  FILES = 'A'..'H'
+
+  def self.valid_square?(rank, file)
+    raise "Please implement the Chess.valid_square? method"
+  end
+
+  def self.nick_name(first_name, last_name)
+    raise "Please implement the Chess.nick_name method"
+  end
+
+  def self.move_message(first_name, last_name, square)
+    raise "Please implement the Chess.move_message method"
+  end
+end
