ba06c707c39595e1123fc2d7c576fb7f16de172d
Nico Rithner <nicorithner@gmail.com>
2023-10-19 20:29:16 -0600

    Solve first test

---
+++ Exercism/ruby/flatten-array/flatten_array.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Write your code for the 'Flatten Array' exercise in this file. Make the tests in
+# `flatten_array_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/flatten-array` directory.
+class FlattenArray
+  def self.flatten(array)
+    array.flatten
+  end
+end
