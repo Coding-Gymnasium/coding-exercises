b4297326010a2e40578d4909f007fafb025245e4
Nico Rithner <nicorithner@gmail.com>
2023-11-07 20:19:47 -0700

    Solve first method

--- Exercism/ruby/space-age/space_age.rb
+++ Exercism/ruby/space-age/space_age.rb
@@ -1,7 +1,16 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
+# frozen_string_literal: true
 
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
+# Write your code for the 'Space Age' exercise in this file. Make the tests in
+# `space_age_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/space-age` directory.
+class SpaceAge
+  def initialize(age)
+    @age = age
+  end
+
+  def on_earth
+    (@age / 31_557_600.to_f).round(2)
+  end
+end
