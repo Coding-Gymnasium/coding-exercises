6487e8bd3c0157ef96cf910cd4283aef39cc6939
Nico Rithner <nicorithner@gmail.com>
2023-11-07 20:30:38 -0700

    Complete methods

--- Exercism/ruby/space-age/space_age.rb
+++ Exercism/ruby/space-age/space_age.rb
@@ -8,9 +8,45 @@
 class SpaceAge
   def initialize(age)
     @age = age
+    @earth = 31_557_600.to_f
   end
 
   def on_earth
-    (@age / 31_557_600.to_f).round(2)
+    (@age / @earth).round(2)
+  end
+
+  def on_mercury
+    mercury = @earth * 0.2408467
+    (@age / mercury).round(2)
+  end
+
+  def on_venus
+    venus = @earth * 0.61519726
+    (@age / venus).round(2)
+  end
+
+  def on_mars
+    mars = @earth * 1.8808158
+    (@age / mars).round(2)
+  end
+
+  def on_jupiter
+    jupiter = @earth * 11.862615
+    (@age / jupiter).round(2)
+  end
+
+  def on_saturn
+    saturn = @earth * 29.447498
+    (@age / saturn).round(2)
+  end
+
+  def on_uranus
+    uranus = @earth * 84.016846
+    (@age / uranus).round(2)
+  end
+
+  def on_neptune
+    neptune = @earth * 164.79132
+    (@age / neptune).round(2)
   end
 end
