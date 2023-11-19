5a4e1bde1a59d471986fb4274708734dd86f6730
Nico Rithner <nicorithner@gmail.com>
2023-11-19 16:09:28 -0700

    Solve when series length is and str length is same

--- Exercism/ruby/series/series.rb
+++ Exercism/ruby/series/series.rb
@@ -10,10 +10,16 @@ class Series
     @number = number
   end
 
-  def slices(length)
-    # @number.split(" ")
+  def slices(series_length)
+    # compare the num_arr length to the series length and substract the difference
+    # If it's equal return the original in an array.
     num_arr = @number.chars
-    puts num_arr.length
-    num_arr
+    return num_arr if series_length == 1
+
+    # If there is a difference push into an array the slice matching the series_length number. Then move to the next index up and repeat until for the number of times that equals the difference.
+    diff = num_arr.length - series_length
+    puts "Diff: #{diff}"
+    puts "Sliced:  #{@number.slice(0, series_length)}"
+    [@number] if diff.zero?
   end
 end
