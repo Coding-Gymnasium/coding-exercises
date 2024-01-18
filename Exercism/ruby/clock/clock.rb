f78aa7fa51bf095d0c8eadc5e60ed3cf87f63a04
Nico Rithner <nicorithner@gmail.com>
2024-01-17 17:17:11 -0700

    Solve hour and minutes roll over

--- Exercism/ruby/clock/clock.rb
+++ Exercism/ruby/clock/clock.rb
@@ -4,12 +4,26 @@
 # To get started with TDD, see the `README.md` file in your
 # `ruby/clock` directory.
 class Clock
+  MINUTES_PER_DAY = 60 * 24
+
   def initialize(hour: 0, minute: 0)
     @hour = hour
     @minute = minute
   end
 
+  def total_minutes
+    ((@hour * 60) + @minute) % MINUTES_PER_DAY
+  end
+
+  def hour
+    total_minutes.div(60)
+  end
+
+  def minutes
+    total_minutes % 60
+  end
+
   def to_s
-    format("%<hour>.2d:%<minute>.2d", hour: @hour, minute: @minute)
+    format("%<hour>.2d:%<minute>.2d", hour: hour, minute: minutes)
   end
 end
