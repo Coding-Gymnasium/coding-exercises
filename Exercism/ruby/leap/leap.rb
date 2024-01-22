37cb0852c0eb1c390ddc46b69fbc0f0b6d72e5cc
Nico Rithner <nicorithner@gmail.com>
2024-01-21 22:45:57 -0700

    Solve leap exercise

---
+++ Exercism/ruby/leap/leap.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "leap_test"
+##
+# A leap year (in the Gregorian calendar) occurs:
+# In every year that is evenly divisible by 4.
+# Unless the year is evenly divisible by 100,
+# in which case it's only a leap year if the year is also evenly divisible by 400
+##
+class Year < Date
+  def self.leap?(number)
+    (number % 400).zero?
+    (number % 100).zero? && (number && 400).zero?
+    (number % 4).zero? unless (number % 100).zero? && (number % 400) != 0
+  end
+end
