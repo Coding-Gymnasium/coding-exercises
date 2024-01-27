f6d0299d32e7701cbb73b0fd5b4d4ba903d597e7
Nico Rithner <nicorithner@gmail.com>
2024-01-27 07:16:16 -0700

    Complete leap year exercise

--- Exercism/javascript/leap/leap.js
+++ Exercism/javascript/leap/leap.js
@@ -1,8 +1,9 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// A leap year (in the Gregorian calendar) occurs:
+// In every year that is evenly divisible by 4.
+// Unless the year is evenly divisible by 100, in which case it's only a leap year if the year is also evenly divisible by 400.
 
 export const isLeap = (year) => {
-  return year % 4 == 0;
+  const leapYear = year % 4 === 0 && year % 100 !== 0;
+  const leapYear400 = year % 100 === 0 && year % 400 === 0;
+  return leapYear || leapYear400;
 };
