fa9b70797e429fbcf2aa05320d10e4b7dd5337fb
Nico Rithner <nicorithner@gmail.com>
2024-02-22 20:37:29 -0700

    Set up salary-calculator

---
+++ Exercism/JAVA/salary-calculator/src/main/java/SalaryCalculator.java
@@ -0,0 +1,17 @@
+public class SalaryCalculator {
+  public double salaryMultiplier(int daysSkipped) {
+
+  }
+
+  public int bonusMultiplier(int productsSold) {
+    throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusMultiplier() method");
+  }
+
+  public double bonusForProductsSold(int productsSold) {
+    throw new UnsupportedOperationException("Please implement the SalaryCalculator.bonusForProductsSold() method");
+  }
+
+  public double finalSalary(int daysSkipped, int productsSold) {
+    throw new UnsupportedOperationException("Please implement the SalaryCalculator.finalSalary() method");
+  }
+}
