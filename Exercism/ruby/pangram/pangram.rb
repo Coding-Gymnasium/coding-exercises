2db857ff19d1ffd73edc9afd003aaaba93146a24
Nico Rithner <nicorithner@gmail.com>
2024-03-05 20:25:35 -0700

    Solve first test

--- Exercism/ruby/pangram/pangram.rb
+++ Exercism/ruby/pangram/pangram.rb
@@ -1,7 +1,11 @@
-=begin
-Write your code for the 'Pangram' exercise in this file. Make the tests in
-`pangram_test.rb` pass.
+# Write your code for the 'Pangram' exercise in this file. Make the tests in
+# `pangram_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/pangram` directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/pangram` directory.
-=end
+class Pangram
+  def self.pangram?(sentence)
+    nil if sentence.empty?
+  end
+end
