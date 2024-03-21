92176a2b5b048ab5a6e5e55296584354facc577e
Nico Rithner <nicorithner@gmail.com>
2024-03-20 20:08:12 -0600

    Solve orient method

--- Exercism/ruby/robot-simulator/robot_simulator.rb
+++ Exercism/ruby/robot-simulator/robot_simulator.rb
@@ -1,7 +1,13 @@
-=begin
-Write your code for the 'Robot Simulator' exercise in this file. Make the tests in
-`robot_simulator_test.rb` pass.
+# Write your code for the 'Robot Simulator' exercise in this file. Make the tests in
+# `robot_simulator_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/robot-simulator` directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/robot-simulator` directory.
-=end
+class Robot
+  attr_reader :bearing
+
+  def orient(direction)
+    @bearing = direction
+  end
+end
